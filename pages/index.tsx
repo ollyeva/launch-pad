efedf45107bb3fbab6127119f8c3b8c5d82ace10
Olly <ollydevans@gmail.com>
2023-01-03 16:26:39 +1100

    Added helpful links to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,16 @@ const Holder = styled.div`
 export default function Home() {
     return (
         <Holder>
-            <p>Hello Launch Pad!</p>
+            <p>Hello noob, here is your basic setup to get you going!</p>
+            <p>Some quick links</p>
+            <ul>
+                <li><a target='_blank' rel='noreferrer noopener'
+                       href='https://nextjs.org/docs'>Next</a></li>
+                <li><a target='_blank' rel='noreferrer noopener'
+                       href='https://docs.pmnd.rs/react-three-fiber/getting-started/introduction'>R3F</a></li>
+                <li><a target='_blank' rel='noreferrer noopener'
+                       href='https://github.com/ollyeva?tab=repositories'>Repos</a></li>
+            </ul>
         </Holder>
     )
 }
